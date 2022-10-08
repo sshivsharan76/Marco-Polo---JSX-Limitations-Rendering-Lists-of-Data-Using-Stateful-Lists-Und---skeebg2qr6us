53fc0b513c2f8c3f17de5a821334681e7097421c
shubham shivsharan <92040473+sshivsharan76@users.noreply.github.com>
2022-10-08 11:51:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,21 @@ import '../styles/App.css';
 const App = () => {
   
   return (
-    <div id="main">
-
+     <div id="main">
+      <h1 id="marco-polo">Marco</h1>
+      <button id="marco-polo-toggler" onClick={onclickHandler}>
+        Polo
+      </button>
     </div>
+  );
   )
 }
+  const onclickHandler = () => {
+  let a = document.getElementById("marco-polo");
+  a.innerText === "Marco" ? (a.innerText = "Polo") : (a.innerText = "Marco");
+
+  let b = document.getElementById("marco-polo-toggler");
+  b.innerText === "Polo" ? (b.innerText = "Marco") : (b.innerText = "Polo");
 
 
 export default App;
